9ec4693a60cf369ab5633af3ba761281d752f0d2
Ian Buttimer <ibuttimer@yahoo.com>
2023-07-11 15:49:40 +0100

    feat: log app start arguments

--- spring-cloud-config-server/src/main/java/com/example/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/example/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -1,15 +1,25 @@
 package com.example.microservices.springcloudconfigserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import java.util.Arrays;
+
 @EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringCloudConfigServerApplication.class);
+
 	public static void main(String[] args) {
+		log.info(
+				String.format("Application starting with command-line arguments: %s.%n" +
+						"To kill this application, press Ctrl + C.", Arrays.toString(args))
+		);
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 	}
 
